e817b650ebc9c8de28184ea3c009dbbe02f0daec
unknown <menachem1988@gmail.com>
2020-01-26 00:05:57 +0200

    img height for mobile

--- src/app/components/drivers/driver/driver.component.ts
+++ src/app/components/drivers/driver/driver.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { Driver } from '../../../model/driver';
+
+
 
 @Component({
   selector: 'app-driver',
@@ -7,16 +9,30 @@ import { Driver } from '../../../model/driver';
   styleUrls: ['./driver.component.scss']
 })
 export class DriverComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (event.target.innerWidth <= 600) {
+        this.img.nativeElement.style.height=this.img.nativeElement.width*1.2+'px';
+
+
+    }
+  }
+
 
   @Input()
-  driver:Driver;
+  driver: Driver;
+  @ViewChild("img") img: ElementRef;
   constructor() { }
 
   ngOnInit() {
     // this.driver.profile_image
   }
-getImageSrc(){
-  return this.driver.profile_image?this.driver.profile_image:"https://i.imgur.com/S2IRHcz.jpg";
-}
+  getImageSrc() {
+    return this.driver.profile_image ? this.driver.profile_image : "https://i.imgur.com/S2IRHcz.jpg";
+  }
+
+  initImgHeight(width:number):number{
+    return width*1.2;
+  }
 
 }
